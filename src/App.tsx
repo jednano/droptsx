5eef1d2804bbfd44bea63d8f3bb15edfb6e74d3a
Jed Mao <jedmao@outlook.com>
2018-01-26 22:44:39 -0600

    Introduce @jedmao/react-bem

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
+import { createBEMBlock } from '@jedmao/react-bem';
 import * as React from 'react';
-import './App.css';
+import './app.css';
 
 const logo = require('./logo.svg');
 
 class App extends React.Component {
+	static defaultProps = {
+		bemBlock: 'app',
+	};
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
+			<div>
+				<header bemElement="header">
+					<img src={logo} bemElement="logo" alt="logo" />
+					<h1 bemElement="title">Welcome to React</h1>
 				</header>
-				<p className="App-intro">
+				<p bemElement="intro">
 					To get started, edit <code>src/App.tsx</code> and save to reload.
 				</p>
 			</div>
@@ -19,4 +23,4 @@ class App extends React.Component {
 	}
 }
 
-export default App;
+export default createBEMBlock(App);
