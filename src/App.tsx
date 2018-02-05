a30551e12de227cbdca56343dfb848ef4c17d131
Jed Mao <jedmao@outlook.com>
2018-02-05 02:20:42 -0600

    @jedmao/react-bem -> bem-join

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import { createBEMBlock } from '@jedmao/react-bem';
+import bemJoin from 'bem-join';
 import * as React from 'react';
 import './app.css';
 
 const logo = require('./logo.svg');
 
-class App extends React.Component {
-	static defaultProps = {
-		bemBlock: 'app',
-	};
+const b = bemJoin('app');
+export default class App extends React.Component {
 	render() {
 		return (
-			<div>
-				<header bemElement="header">
-					<img src={logo} bemElement="logo" alt="logo" />
-					<h1 bemElement="title">Welcome to React</h1>
+			<div className={b()}>
+				<header className={b('header')}>
+					<img src={logo} className={b('logo')} alt="logo" />
+					<h1 className={b('title')}>Welcome to React</h1>
 				</header>
-				<p bemElement="intro">
+				<p className={b('intro')}>
 					To get started, edit <code>src/App.tsx</code> and save to reload.
 				</p>
 			</div>
 		);
 	}
 }
-
-export default createBEMBlock(App);
